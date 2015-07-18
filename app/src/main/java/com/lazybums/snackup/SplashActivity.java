3a70a1be3fa06827d9254ff726a2a1572e4cadda
CodeFunk <abhi6691@gmail.com>
2015-07-18 18:37:44 +0530

    Initial screen with check for internet connectivity

--- app/src/main/java/com/lazybums/snackup/SplashActivity.java
+++ app/src/main/java/com/lazybums/snackup/SplashActivity.java
@@ -1,17 +1,96 @@
 package com.lazybums.snackup;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class SplashActivity extends Activity {
+    AlertDialog dialog;
+    AlertDialog.Builder build;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        build = new AlertDialog.Builder(this);
+        if (NetworkManager.isConnected(this)) {
+            Toast.makeText(SplashActivity.this, "Internet is active", Toast.LENGTH_SHORT)
+                    .show();
+            Thread mythread = new Thread() {
+                public void run() {
+                    try {
+
+                        sleep(5000);
+
+                    } catch (Exception e) {
+                    } finally {
+                        Intent intent = new Intent(SplashActivity.this,
+                                LandingActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            };
+            mythread.start();
+        } else {
+
+            build.setMessage("This application requires Internet connection.");
+            build.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+                    finish();
+                    startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+
+                }
+            });
+            build.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // TODO Auto-generated method stub
+                    build.setMessage("Are sure you want to exit?");
+                    build.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // TODO Auto-generated method stub
+                            finish();
+                        }
+                    });
+                    build.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // TODO Auto-generated method stub
+                            finish();
+                            Intent intent = new Intent(SplashActivity.this,
+                                    SplashActivity.class);
+                            startActivity(intent);
+
+                            dialog.dismiss();
+
+                        }
+                    });
+                    //dialog = build.create();
+                   // dialog.
+                }
+            });
+            dialog = build.create();
+            dialog.show();
+
+        }
     }
 
 
--- app/src/main/java/com/lazybums/snackup/VendorActivity.java
+++ app/src/main/java/com/lazybums/snackup/VendorActivity.java
@@ -5,4 +5,5 @@ package com.lazybums.snackup;
  */
 public class VendorActivity {
 
+
 }
