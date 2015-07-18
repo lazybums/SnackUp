633aee5cf72d0e939a6d5dc4bc5437a48c086a5d
CodeFunk <abhi6691@gmail.com>
2015-07-18 16:58:05 +0530

    Initial skeleton for the app

--- app/src/main/java/com/lazybums/snackup/LandingActivity.java
+++ app/src/main/java/com/lazybums/snackup/LandingActivity.java
@@ -3,6 +3,5 @@ package com.lazybums.snackup;
 /**
  * Created by Abhishek on 7/18/2015.
  */
-public class VendorActivity {
-
+public class LandingActivity {
 }
--- app/src/main/java/com/lazybums/snackup/SplashActivity.java
+++ app/src/main/java/com/lazybums/snackup/SplashActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+//hey guys let's go
 public class SplashActivity extends Activity {
 
     @Override
